a78c8be84478f1f8df1a2d42a9bd6adcc4eb4b9d
angel2s2 <angel2s2ru@gmail.com>
2013-11-27 14:41:54 +0300

    Добавил мускул и отсортировал по папкам

---
+++ postfix/scripts/postfix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MAILLOG=/var/log/maillog
+DAT1=/tmp/zabbix-postfix-offset.dat
+DAT2=$(mktemp)
+PFLOGSUMM=/usr/sbin/pflogsumm
+ZABBIX_CONF=/etc/zabbix/zabbix_agentd.conf
+DEBUG=0
+
+function zsend {
+  key="postfix[`echo "$1" | tr ' -' '_' | tr '[A-Z]' '[a-z]' | tr -cd [a-z_]`]"
+  value=`grep -m 1 "$1" $DAT2 | awk '{print $1}'`
+  [ ${DEBUG} -ne 0 ] && echo "Send key "${key}" with value "${value}"" >&2
+  /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}" 2>&1 >/dev/null
+}
+
+/usr/sbin/logtail -f$MAILLOG -o$DAT1 2>/dev/null | $PFLOGSUMM -h 0 -u 0 --no_bounce_detail --no_deferral_detail --no_reject_detail --no_no_msg_size --no_smtpd_warnings > $DAT2
+
+zsend received
+zsend delivered
+zsend forwarded
+zsend deferred
+zsend bounced
+zsend rejected
+zsend held
+zsend discarded
+zsend "reject warnings"
+zsend "bytes received"
+zsend "bytes delivered"
+zsend senders
+zsend recipients
+
+rm $DAT2
