0f4a1055a1ded0df39ff970eb6818a9c004485c7
angel2s2 <angel2s2ru@gmail.com>
2013-12-10 16:53:07 +0300

    Добавил в скрипт и шаблон postfix параметр "connections"

--- postfix/scripts/postfix.sh
+++ postfix/scripts/postfix.sh
@@ -12,16 +12,17 @@ function zsend {
   value=`grep -m 1 "$1" $DAT2 | awk '{print $1}'`
   if [ ${DEBUG} -ne 0 ] ; then
     echo "Send key "${key}" with value "${value}"" >&2
-    /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}" -vv
+    /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}" 
   else
     /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}" 2>&1 >/dev/null
   fi
 }
 
-/usr/sbin/logtail -f$MAILLOG -o$DAT1 | $PFLOGSUMM -h 0 -u 0 --no_bounce_detail --no_deferral_detail --no_reject_detail --no_no_msg_size --no_smtpd_warnings > $DAT2
+/usr/sbin/logtail -f$MAILLOG -o$DAT1 | $PFLOGSUMM -h 0 -u 0 --smtpd_stats --no_bounce_detail --no_deferral_detail --no_reject_detail --no_no_msg_size --no_smtpd_warnings > $DAT2
 
 zsend received
 zsend delivered
+zsend connections
 zsend forwarded
 zsend deferred
 zsend bounced
@@ -29,9 +30,9 @@ zsend rejected
 zsend held
 zsend discarded
 zsend "reject warnings"
-zsend "bytes received"
-zsend "bytes delivered"
 zsend senders
 zsend recipients
+zsend "bytes received"
+zsend "bytes delivered"
 
 rm $DAT2
